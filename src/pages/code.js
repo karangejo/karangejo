2a7812372b54ba4b28ef00ed9b115eb0807fd5a8
karang <gabriel.g.gras@gmail.com>
2020-04-15 17:02:31 +0800

    added some syntax highlighting

--- src/pages/code.js
+++ src/pages/code.js
@@ -1,17 +1,52 @@
 import React from 'react'
+import {Grid} from '@material-ui/core';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import { duotoneSea as codeStyle } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Layout from '../components/layout';
 import Boat from '../images/battleship.png';
 import CelticRight from '../components/celticRight';
 import CelticLeft from '../components/celticLeft';
 
 function Code(){
-   
+      const codeString = 
+      `import {useEffect, useRef } from 'react';
+
+      function useInterval(callback, delay) {
+        const savedCallback = useRef();
+      
+        // Remember the latest callback.
+        useEffect(() => {
+          savedCallback.current = callback;
+        }, [callback]);
+      
+        // Set up the interval.
+        useEffect(() => {
+          function tick() {
+            savedCallback.current();
+          }
+          if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+          }
+        }, [delay]);
+      }
+      
+      export default useInterval;`
 
       return(
             <Layout boatImage={Boat} imageWidth="35vw" borderRight={<CelticRight alignItems="flex-start"/>} borderLeft={<CelticLeft alignItems="flex-end"/>} >
-                  <h1>
-                        Code
-                  </h1>
+                  <Grid container direction="column" justify="center" alignItems="center" style={{paddingRight: "10px", paddingLeft: "10px", width: "60vw"}}>
+                        <Grid item>
+                              <h1>
+                                    Code
+                              </h1>
+                        </Grid>
+                        <Grid item>
+                              <SyntaxHighlighter className="code-block" language="javascript" style={codeStyle} wrapLines={true}>
+                                    {codeString}
+                              </SyntaxHighlighter>
+                        </Grid>
+                  </Grid>
             </Layout>
       )
         
--- src/pages/samplePage.js
+++ src/pages/samplePage.js
@@ -1,18 +1,36 @@
 import React from 'react'
+import {Grid} from '@material-ui/core';
 import Layout from '../components/layout';
 import Boat from '../images/boat-1297127.png';
+import KnotsLeft from '../components/knotsLeft';
+import KnotsRight from '../components/knotsRight';
 
 function Home(){
    
 
       return(
-            <Layout boatImage={Boat} imageWidth="35vw">
-                  <h1>
-                        Home
-                  </h1>
+      <Layout boatImage={Boat} imageWidth="35vw" borderRight={<KnotsRight/>} borderLeft={<KnotsLeft/> }>
+                  <Grid container direction="column" justify="center" alignItems="center" style={{paddingRight: "10px", paddingLeft: "10px", width: "60vw"}}>
+                        <Grid item>
+                              <h1>
+                                    Home
+                              </h1>
+                        </Grid>
+                        <Grid item>
+                              <p>
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                              </p>
+                        </Grid>
+                  </Grid>
             </Layout>
       )
         
       }
 
 export default Home
+
+
+// white and blue 
+//linear-gradient(90deg, rgba(152,193,217,1) 0%, rgba(254,251,234,1) 20%, rgba(254,251,234,1) 80%, rgba(152,193,217,1) 100%)
+// white and yellow
+//linear-gradient(90deg, rgba(236,223,149,1) 0%, rgba(254,251,234,1) 20%, rgba(254,251,234,1) 80%, rgba(236,223,149,1) 100%)
